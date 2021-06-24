3efbea6d807d6fd2a33d2b8607a0a125d4055165
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-06-24 10:16:23 +0530

    Fixed Bug

--- config.py
+++ config.py
@@ -1,3 +1,21 @@
+# Regen & Mod by @shamilhabeebnelli
+# Pyrogram - Telegram MTProto API Client Library for Python
+# Copyright (C) 2017-2020 Dan <https://github.com/delivrance>
+#
+# This file is part of Pyrogram.
+#
+# Pyrogram is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pyrogram is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import re
@@ -9,7 +27,7 @@ ydl_opts = {
 ydl = YoutubeDL(ydl_opts)
 links=[]
 finalurl=""
-STREAM=os.environ.get("STREAM_URL", "https://youtu.be/W9NyVYHtrIU") # 24x7 Tamil Romantic Hits
+STREAM=os.environ.get("STREAM_URL", "http://node-25.zeno.fm/kezsc0y2wwzuv?listening-from-radio-garden=1622271954020&rj-ttl=5&rj-tok=AAABec5bAE4Aj31dmRAEFgcbvw")
 regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+"
 match = re.match(regex,STREAM)
 if match:
@@ -32,6 +50,7 @@ class Config:
     else:
         LOG_GROUP=None
     STREAM_URL=finalurl
+    ADMIN_ONLY=os.environ.get("ADMIN_ONLY", "N")
     ARQ_API=os.environ.get("ARQ_API", "")
     DURATION_LIMIT=int(os.environ.get("DUR", 15))
     API_HASH = os.environ.get("API_HASH", "")
@@ -39,4 +58,3 @@ class Config:
     SESSION = os.environ.get("SESSION_STRING", "")
     playlist=[]
     msg = {}
-
