a0e2dce62c7cfab4f60a3f2c1f5d74990b7d306f
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-07-15 14:43:37 +0530

    Update commands.py

--- shamil/commands.py
+++ shamil/commands.py
@@ -31,32 +31,12 @@ CHAT=Config.CHAT
 
 HOME_TEXT = "<b>Helo, [{}](tg://user?id={})\n\n• Iam A Bot Project by MwK MusicS\n• I Can Manage Group VC's\n\n• Hit /help to know about available commands.</b>"
 HELP = """
-🎧 <b>I Can Play Musics On VoiceChats 🤪</b>
-
-🎶 **Common Commands**:
-• `/song` __Download Song from youtube__
-• `/c`  __Show current playing song__
-• `/help` __Show help for commands__
-• `/mwk` __Shows the playlist__
-• `/stickerid` __To Get Id Of Replied Sticker__
+🎧 <b>I Can Play Music On VoiceChats 🤪</b>
 
 🎶 **Admin Commands**:
-• `/p`  __Reply to an audio file or YouTube link to play it or use /p <song name>__
-• `/d` __Play music from Deezer, Use /d <song name>__
-• `/sk [n]` __...Skip current or n where n >= 2__
-• `/j`  __Join voice chat__
-• `/l`  __Leave current voice chat__
-• `/mwk`  __Check which VC is joined__
-• `/sp`  __Stop playing__
+
 • `/r` __Start Radio__
 • `/sr` __Stops Radio Stream__
-• `/rp`  __Play from the beginning__
-• `/cl`  __Remove unused RAW PCM files__
-• `/ps` __Pause playing__
-• `/rs` __Resume playing__
-• `/m`  __Mute in VC__
-• `/um`  __Unmute in VC__
-• `/update` __Update Current Settings n Restarts the Bot__
 
 © Powered By 
 [ __@mwklinks | @redbullfed__ ]
