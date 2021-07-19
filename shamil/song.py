b898e33c6fc95bcdff744ec4ff7bcbb17a10d0b7
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-07-19 17:07:37 +0530

    Update song.py

--- shamil/song.py
+++ shamil/song.py
@@ -18,9 +18,7 @@ def time_to_seconds(time):
 
 @Client.on_message(filters.text)
 def a(client, message):
-    query = ''
-    for i in message.text[1:]:
-        query += ' ' + str(i)
+	query=message.text
     print(query)
     m = message.reply('🧐')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
