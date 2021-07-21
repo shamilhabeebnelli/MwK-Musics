0e2df5470abbf6f327579ad316ade6a7874d8462
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-07-21 22:32:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,9 +51,6 @@ class Config:
         LOG_GROUP=None
     STREAM_URL=finalurl
     ADMIN_ONLY=os.environ.get("ADMIN_ONLY", "Y")
-    ARQ_API=os.environ.get("ARQ_API", "")
-    REPLY_MESSAGE=os.environ.get("REPLY_MESSAGE", None)
-    DURATION_LIMIT=int(os.environ.get("DUR", 15))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     SESSION = os.environ.get("SESSION_STRING", "SESSION")
