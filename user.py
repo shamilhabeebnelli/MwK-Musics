ffb2f6cdce115eaab5d37101a06658ccf8d32f3f
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-07-22 06:46:39 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,9 +1,9 @@
 from config import Config
 from pyrogram import Client
-from config import Config
-    USER = Client(
-        Config.SESSION,
-        Config.API_ID,
-        Config.API_HASH
-        )
+
+USER = Client(
+    Config.SESSION,
+    Config.API_ID,
+    Config.API_HASH
+)
 USER.start()
