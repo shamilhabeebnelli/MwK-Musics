ae99f7f12d1c7e11be222a4247f5a718fdd85bb6
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-09-25 20:54:54 +0530

    Update radio.py

--- shamil/radio.py
+++ shamil/radio.py
@@ -6,7 +6,7 @@ from config import STREAM
 
 ADMINS=Config.ADMINS
 
-@Client.on_message(filters.command(["r", f"r@{USERNAME}"]) & filters.user(ADMINS))
+@Client.on_message(filters.command(["radio", f"radio@{USERNAME}"]) & filters.user(ADMINS))
 async def radio(client, message: Message):
     if 1 in RADIO:
         await message.reply_text("Kindly stop existing Radio Stream /sr")
@@ -15,7 +15,7 @@ async def radio(client, message: Message):
     await message.reply_text(f"Started Radio: <code>{STREAM}</code>")
     await message.delete()
 
-@Client.on_message(filters.command(['sr', f"sr@{USERNAME}"]) & filters.user(ADMINS))
+@Client.on_message(filters.command(['stopradio', f"stopradio@{USERNAME}"]) & filters.user(ADMINS))
 async def stop(_, message: Message):
     if 0 in RADIO:
         await message.reply_text("Kindly start Radio First /r")
